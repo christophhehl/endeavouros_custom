c4228f94c69a8344b4886cf9696431629634a000
Christoph Hehl <chehl@marco.de>
2025-04-29 17:57:01 +0200

    refactor: extract code into functions

--- install_desktop.sh
+++ install_desktop.sh
@@ -8,45 +8,49 @@ create_default_folders() {
   echo "All folders created."
 }
 
-if command -v yay &>/dev/null; then
-  echo "Yay is already installed."
-else
-  echo "Installing yay..."
+install_yay() {
+  if command -v yay &>/dev/null; then
+    echo "Yay is already installed."
+  else
+    echo "Installing yay..."
+    # Install dependencies
+    sudo pacman -S --needed --noconfirm base-devel git
 
-  # Install dependencies
-  sudo pacman -S --needed --noconfirm base-devel git
+    # Clone the yay repository
+    git clone https://aur.archlinux.org/yay.git
+    {
+      cd yay || echo "yay was not downloaded correctly." && exit 1
+      # Build and install yay
+      makepkg -si --noconfirm
+    }
 
-  # Clone the yay repository
-  git clone https://aur.archlinux.org/yay.git
+    # Clean up the yay source directory
+    rm -rf yay
+    echo "yay installation complete."
+  fi
+}
 
-  {
-    cd yay || echo "yay was not downloaded correctly." && exit 1
+change_login_shell() {
+  current_login_shell=$(getent passwd "$USER" | awk -F':' '{print $7}')
+  if [[ "$current_login_shell" != "/bin/zsh" ]]; then
+    echo "Changing shell to zsh."
+    chsh -s /bin/zsh
+  else
+    echo "Login shell already is zsh."
+  fi
+}
 
-    # Build and install yay
-    makepkg -si --noconfirm
-  }
+# ===== begin installation =====
+install_yay
 
-  rm -rf yay # Clean up the yay source directory
-
-  echo "yay installation complete."
-fi
-
+# install zsh (if needed)
 yay -S --needed --noconfirm zsh
 
-current_login_shell=$(getent passwd "$USER" | awk -F':' '{print $7}')
-
-if [[ "$current_login_shell" != "/bin/zsh" ]]; then
-  echo "Changing shell to zsh."
-  chsh -s /bin/zsh
-else
-  echo "Login shell already is zsh."
-fi
-
+change_login_shell
 create_default_folders
 
 echo "Installing default packages."
 yay -S --needed --noconfirm bitwarden discord dropbox eza google-chrome jetbrains-toolbox kitty lazygit localsend-bin neovim obs-studio obsidian ripgrep spotify teamspeak ttf-jetbrains-mono vlc
-
 echo "Installation complete."
 
 echo "Setting configuration files."
